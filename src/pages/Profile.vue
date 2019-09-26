c9fc8ea3c24ccca98796c9390abdd0a6951506ea
Frederik Dietz <fdietz@gmail.com>
2019-09-26 17:42:05 +0200

    init

---
+++ src/pages/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <h1>My Profile</h1>
+  </Layout>
+</template>
+
+<script>
+  export default {
+    metaInfo: {
+      title: "My Profile"
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/templates/Post.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <h1 v-html="$page.post.title"></h1>
+    <div v-html="$page.post.body" />
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.post.title
+    }
+  }
+}
+</script>
+
+<page-query>
+query Post($id: ID!) {
+  post(id: $id) {
+    title
+    body
+  }
+}
+</page-query>
